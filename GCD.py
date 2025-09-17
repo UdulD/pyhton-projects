720d3ecd2b69d62025ad3f5e90230bd206eb07cb
udulD <uduldilsara123@gmail.com>
2025-09-17 14:10:24 +0530

    GCD finder with recursion

---
+++ GCD.py
@@ -0,0 +1,18 @@
+def GCD(a,b):
+    if a>b:
+        n=a//b
+        c=a-n*b
+        if c==0:
+            return b
+        return GCD(b,c)
+    else:
+        n=b//a
+        c=b-n*a  
+        if c==0:
+            return a
+        return GCD(a,c)
+
+a = int(input('enter a: '))
+b = int(input('enter b: '))
+
+print(GCD(a, b))
