a666c7ae779eb58550fa0f69e48500c63856d0b7
udulD <uduldilsara123@gmail.com>
2025-09-18 15:19:35 +0530

    stack implementation

---
+++ stack.py
@@ -0,0 +1,37 @@
+stack=[]
+
+def push(x):
+    stack.append(int(input('enter x ')))
+
+def pop(x):
+    if len(stack)==0:
+        print('stack is emty')
+    else:
+        stack.pop()
+
+def peek(x):
+    if len(stack)==0:
+        print('emty stack')
+    else:
+        print(stack[-1])
+
+def display(x):
+    if len(stack)==0:
+        print('emty stack')
+    else:
+        print(stack)
+x=True
+while True:
+    choice = input("Enter your choice: ")
+
+    if choice == "push":
+        push(x)
+    elif choice == "pop":
+        pop(x)
+    elif choice == "peek":
+        peek(x)
+    elif choice == "dis":
+        display(x)
+    
+
+
