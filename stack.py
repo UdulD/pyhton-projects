50f86fbf80effcbc1ab734ddff2223d392b55bbc
udulD <uduldilsara123@gmail.com>
2025-09-22 10:52:50 +0530

    stack(edited)

--- stack.py
+++ stack.py
@@ -1,3 +1,41 @@
+stack=[]
+
+def add(x):
+    stack.append(int(input('enter value u wanna add ')))
+
+def pop(x):
+    if len(stack)==0: 
+        print('emty stack')
+    else:
+        stack.pop(-1)
+
+def peek(x):
+    if len(stack)==0:
+        print('emty stack')
+    else:
+        print(stack[-1])
+    
+def display(x):
+    if len(stack)==0:
+        print('emty stack')
+    else:
+        print(stack)
+ 
+x=True
+while True:
+    choice =input('enter your choice ')
+    if choice== 'add':
+        add(x)
+    elif choice=='pop':
+        pop(x)
+    elif choice=='peek':
+        peek(x)
+    elif choice=='dis':
+        display(x)
+    elif choice=='exit':
+        break
+    else:
+        print('invalid choice')
 
         
 
