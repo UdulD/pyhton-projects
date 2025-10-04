dff4e682990741aa8cdd9569cbd4f77713fe6ef2
udulD <uduldilsara123@gmail.com>
2025-10-04 15:38:30 +0530

    car class

---
+++ car.py
@@ -0,0 +1,32 @@
+class Car:
+    def __init__(self, fuel_level, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.fuel_level = fuel_level
+
+    def start(self):
+        if self.fuel_level < 2:
+            print("Not enough fuel level")
+        else:
+            print("Starting...")
+
+    def add_fuel(self, litre):
+        if self.fuel_level + litre <= 25:
+            self.fuel_level += litre
+            print(f"Fuel level: {self.fuel_level}")
+        else:
+            print("Maximum fuel level reached")
+
+    def drive(self, distance):
+        if distance > self.fuel_level * 10:
+            print("Not enough fuel")
+        else:
+            print('enough fuel')
+
+
+Car1 = Car(5,'toyota','axio',2014)
+Car1.start()
+Car1.drive(15)
+Car1.add_fuel(5)
+ 
