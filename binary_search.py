23ba29b216f9bfdc1e9963163ab81cb7cd4e2b1f
udulD <uduldilsara123@gmail.com>
2025-10-10 22:00:00 +0530

    binary search

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+def binary(arr,n):
+    lower=0
+    upper=len(arr)-1
+
+    while lower<=upper:
+        mid=(lower+upper)//2
+        if arr[mid]==n:
+            return mid
+        elif arr[mid]>n:
+                upper=mid-1
+        else:
+            lower=mid+1
+            
+    print('not found')
+
+arr=[1,2,3,4,5,6,7,8,9,10,11]
+
+
+print(binary(arr,11))
+
